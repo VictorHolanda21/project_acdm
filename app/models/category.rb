40e2f386c8432d379c0ff6b280df339c1d4cc266
Herald Freire <heraldfreire@gmail.com>
2015-08-06 22:10:56 -0300

    Bootstrap all MVC / Standardized codes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ActiveRecord::Base
+
+	validates_presence_of :name, :description
+	validates_uniqueness_of :name, :description
+	validates_length_of :description, maximum: 150
+
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ActiveRecord::Base
+
+	has_many :students
+
+	validates_presence_of :name, :workload, :value, :description
+	validates_uniqueness_of :name, :workload, :value, :description
+	validates_length_of :description, maximum: 150
+	
 end
